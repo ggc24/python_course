9712d4bb622d2f2d9f62d19e9cc5c85f653ced57
Gabriel González Castro <gabrielgonzalezcastro@gmail.com>
2020-06-02 19:48:27 +0200

    Added module 9 (Inheritance and Subtype Polymorphism)

---
+++ module9/diamond.py
@@ -0,0 +1,16 @@
+class A:
+    def func(self):
+        return 'A.func'
+
+class B:
+    def func(self):
+        return 'B.func'
+
+class C:
+    def func(self):
+        return 'C.func'
+
+class D(B, C):
+    pass
+
+
